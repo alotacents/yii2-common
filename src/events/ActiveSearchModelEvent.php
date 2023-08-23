39f482d75ad979fe81ca9567d686b62d1b385247
alotacents <alotacents@gmail.com>
2023-08-23 10:52:29 -0700

    Create ActiveSearchModelEvent.php

---
+++ src/events/ActiveSearchModelEvent.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @link https://github.com/yii2tech
+ * @copyright Copyright (c) 2015 Yii2tech
+ * @license [New BSD License](http://www.opensource.org/licenses/bsd-license.php)
+ */
+
+namespace alotacents\yii2-common\events;
+
+use yii\base\ModelEvent;
+
+/**
+ * ActiveSearchEvent represents the parameter needed by {@see ActiveSearchModel} events.
+ *
+ * @see ActiveSearchModel
+ *
+ * @author Paul Klimov <klimov.paul@gmail.com>
+ * @since 1.0
+ */
+class ActiveSearchModelEvent extends ModelEvent
+{
+    /**
+     * @var \yii\db\ActiveQueryInterface|null related active query instance.
+     */
+    public $query;
+}
