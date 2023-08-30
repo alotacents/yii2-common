54eaf35a73201165a2622633ced152bbe12d0861
alotacents <alotacents@gmail.com>
2023-08-30 16:26:43 -0700

    Update RoleBehavior.php

--- src/behaviors/RoleBehavior.php
+++ src/behaviors/RoleBehavior.php
@@ -5,7 +5,7 @@
  * @license [New BSD License](http://www.opensource.org/licenses/bsd-license.php)
  */
 
-namespace alotacents\yii2-common\behaviors;
+namespace alotacents\common\behaviors;
 
 use yii\base\Behavior;
 use yii\base\Model;
